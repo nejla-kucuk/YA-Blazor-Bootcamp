df1d77b63c4c3c3f0057920882786b41df424099
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-07-14 19:12:06 +0300

    [Update]: Add Blazor Toast in Program

--- NK/NK.BlazorWasmStarter/Program.cs
+++ NK/NK.BlazorWasmStarter/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NK.BlazorWasmStarter;
@@ -8,4 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddBlazoredToast();
+
 await builder.Build().RunAsync();
+
