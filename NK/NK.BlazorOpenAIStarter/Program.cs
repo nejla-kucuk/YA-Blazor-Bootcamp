c255cb542000ebeb13bb5d7eab299052d27c58f2
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-07-19 18:15:50 +0300

    [Feat]: Add OpenAI config

--- NK/NK.BlazorOpenAIStarter/Program.cs
+++ NK/NK.BlazorOpenAIStarter/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using NK.BlazorOpenAIStarter;
+using OpenAI.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var openAIKey = builder.Configuration.GetSection("OpenAIApiKey").Value;
+
+builder.Services.AddOpenAIService(settings => settings.ApiKey = openAIKey);
 
 await builder.Build().RunAsync();
