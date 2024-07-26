2315f2324950a9e237578fc83107701367e3a4fa
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-07-26 21:07:22 +0300

    Create Program.cs

---
+++ NK/NK.VideoTranscribeApp.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Resources;
+using Xabe.FFmpeg;
+
+var videoFilePath = "C:\\Users\\Asus\\Desktop\\\\Sam-Bankman-Friend.mp4";
+
+string audioFilePath = Path.ChangeExtension(Path.GetTempFileName(), "mp3"); //mp4'ü mp3'e çevirir.
+
+CancellationTokenSource cts = new CancellationTokenSource();
+
+var conversion = await FFmpeg.Conversions.FromSnippet.ExtractAudio(videoFilePath, audioFilePath);
+
+await conversion.Start(cts.Token);
+
+Console.WriteLine("Completed..");
+Console.WriteLine(audioFilePath);
+
