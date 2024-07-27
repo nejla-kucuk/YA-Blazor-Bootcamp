da6695a43d970c411b633f7ce0692b2a9aabbe94
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-07-27 19:10:44 +0300

    [Update]: Change theme

--- NK/NK.BlazorGiftSuggestionApp/tailwind.config.js
+++ NK/NK.BlazorGiftSuggestionApp/tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
       require('@tailwindcss/forms'),
     ],
     daisyui: {
-        themes: ["light", "dark", "cupcake", "synthwave", "valentine"],
+        themes: ["light", "dark", "cupcake", "sunset"],
     }
 }
 
