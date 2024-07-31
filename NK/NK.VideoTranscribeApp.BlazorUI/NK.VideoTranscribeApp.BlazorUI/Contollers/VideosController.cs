fe845e1c6513790293e76c3715db2e52606f506c
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-07-31 10:06:01 +0300

    [Feat]: Add Controller for video

---
+++ NK/NK.VideoTranscribeApp.BlazorUI/NK.VideoTranscribeApp.BlazorUI/Contollers/VideosController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Xabe.FFmpeg;
+
+namespace NK.VideoTranscribeApp.BlazorUI.Contollers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideosController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public VideosController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadAsync(VideoUploadRequest request, CancellationToken cancellationToken)
+        {
+            var uploadsPath = Path.Combine(_environment.WebRootPath, "uploads");
+
+            var videoFilePath = "C:\\Users\\Asus\\Desktop\\Sam-Bankman-Fried.mp4";
+            
+            string audioFilePath = Path.ChangeExtension(videoFilePath, "mp3");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            FFmpeg.SetExecutablesPath("C:\\Users\\Asus\\Downloads\\ffmpeg-master-latest-win64-gpl\\ffmpeg-master-latest-win64-gpl\\bin");
+
+           
+            var conversion = await FFmpeg.Conversions.FromSnippet.ExtractAudio(videoFilePath, audioFilePath);
+
+            await conversion.Start(cts.Token);
+
+            Console.WriteLine("Conversion completed.");
+
+            Console.WriteLine(audioFilePath);
+            return Ok();
+        }
+    }
+
+
+    public class VideoUploadRequest
+    {
+        public IFormFile Video { get; set; }
+        public string[] Languages { get; set; }
+    }
+
+}
