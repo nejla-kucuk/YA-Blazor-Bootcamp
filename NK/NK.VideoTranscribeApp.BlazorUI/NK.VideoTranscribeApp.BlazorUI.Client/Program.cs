b1525ca02e89a525e64f66563335a426eb74180e
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-07-31 09:56:43 +0300

    [Update]: Add project's url

--- NK/NK.VideoTranscribeApp.BlazorUI/NK.VideoTranscribeApp.BlazorUI.Client/Program.cs
+++ NK/NK.VideoTranscribeApp.BlazorUI/NK.VideoTranscribeApp.BlazorUI.Client/Program.cs
@@ -3,3 +3,5 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 await builder.Build().RunAsync();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5038/api/") });
