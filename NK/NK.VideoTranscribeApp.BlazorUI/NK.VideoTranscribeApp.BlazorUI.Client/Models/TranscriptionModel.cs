d2d4ab500b99f6261e9a2164ff39f027f8f7357c
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-08-04 16:36:29 +0300

    [Feat]: Add Model

---
+++ NK/NK.VideoTranscribeApp.BlazorUI/NK.VideoTranscribeApp.BlazorUI.Client/Models/TranscriptionModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NK.VideoTranscribeApp.BlazorUI.Client.Models
+{
+    public class TranscriptionModel
+    {
+        public string Language { get; set; }
+        public string Text { get; set; }
+    }
+}
