b92ff5e70b818293190609eb8c89511a040b4b39
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-08-04 13:29:57 +0300

    [Feat]: Add Dtos

---
+++ NK/NK.PasswordStorageApp.WebAPI/Dtos/AccountCreateDto.cs
@@ -0,0 +1,26 @@
+﻿using NK.PasswordStorageApp.WebAPI.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace NK.PasswordStorageApp.WebAPI.Dtos
+{
+    public class AccountCreateDto
+    {
+        [Required, MinLength(6), MaxLength(25)]
+        public string Username { get; set; }
+
+        [Required, MinLength(6), MaxLength(50)]
+        public string Password { get; set; }
+
+        public Account ToAccount()
+        {
+            return new Account
+            {
+                Id = Ulid.NewUlid().ToGuid(),
+                Username = Username,
+                Password = Password,
+                CreatedOn = DateTime.UtcNow
+            };
+        }
+    }
+
+}
