80b0bf2109019d91380cc1119cac07060eab4b8e
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-08-04 16:35:47 +0300

    [Feat]: Add Button Size Enum

---
+++ NK/NK.VideoTranscribeApp.BlazorUI/NK.VideoTranscribeApp.BlazorUI.Client/Enum/ButtonSize.cs
@@ -0,0 +1,10 @@
+﻿namespace NK.VideoTranscribeApp.BlazorUI.Client.Enum
+{
+    public enum ButtonSize
+    {
+        ExtraSmall = 1,
+        Small = 2,
+        Medium = 3,
+        Large = 4
+    }
+}
