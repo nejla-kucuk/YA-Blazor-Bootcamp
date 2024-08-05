c0fa64e10684b07c3714df4e13a75b2b1aca5ea4
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-08-05 11:57:17 +0300

    [Feat]: Delete

--- NK/NK.VideoTranscribeApp.BlazorUI/NK.VideoTranscribeApp.BlazorUI/Models/TranscriptionModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NK.VideoTranscribeApp.BlazorUI.Models
-{
-    public class TranscriptionModel
-    {
-        public string Language { get; set; }
-        public string Text { get; set; }
-    }
-}
