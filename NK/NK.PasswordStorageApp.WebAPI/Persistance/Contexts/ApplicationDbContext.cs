fe77a82165abe1d1f89706fa32fa884bfaf3f78f
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-08-06 18:38:29 +0300

    Create ApplicationDbContext.cs

---
+++ NK/NK.PasswordStorageApp.WebAPI/Persistance/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NK.PasswordStorageApp.Domain.Model;
+
+namespace NK.PasswordStorageApp.WebAPI.Persistance.Contexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.ApplyConfiguration(new AccountConfiguration());
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly); //Entity config. otomatik olusturur.
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlite("Data Source=PasswordStorageApp.db;");
+        }
+    }
+}
