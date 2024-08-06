b53102ce7acc6da8cf4e6c1d5e62594ebdcfec61
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-08-06 18:35:53 +0300

    [Feat]: Add Model

---
+++ NK/NK.PasswordStorageApp.Domain/Model/Account.cs
@@ -0,0 +1,19 @@
+﻿using NK.PasswordStorageApp.Domain.Enums;
+
+namespace NK.PasswordStorageApp.Domain.Model
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set;}
+
+        public string Password { get; set;}
+
+        public AccountType Type { get; set;}
+
+        public DateTimeOffset CreatedOn { get; set; }
+
+        public DateTimeOffset? ModifiedOn { get; set; }
+    }
+}
