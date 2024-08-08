3e79dc3c55576b9a7b2ccabe0fa908584e318587
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-08-08 23:51:31 +0300

    [Feat]: Create Dto For GetAll Method

---
+++ NK/NK.PasswordStorageApp.Domain/Dtos/AccountGetAllDto.cs
@@ -0,0 +1,32 @@
+﻿using NK.PasswordStorageApp.Domain.Enums;
+using NK.PasswordStorageApp.Domain.Model;
+
+
+namespace NK.PasswordStorageApp.Domain.Dtos
+{
+    public class AccountGetAllDto
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public AccountType Type { get; set; }
+
+        public DateTimeOffset CreatedOn { get; set; }
+
+
+        public static AccountGetAllDto MapFromAccount(Account account)
+        {
+            return new AccountGetAllDto
+            {
+                Id = account.Id,
+                Username = account.Username,
+                Password = account.Password,
+                Type = account.Type,
+                CreatedOn = account.CreatedOn
+            };
+        }
+    }
+}
