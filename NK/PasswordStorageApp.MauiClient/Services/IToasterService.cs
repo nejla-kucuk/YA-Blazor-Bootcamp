ec7343d7ba26199a141bd1930535a16de3e3cad0
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-15 17:01:27 +0300

    [Feat]: Create IToasterService

---
+++ NK/PasswordStorageApp.MauiClient/Services/IToasterService.cs
@@ -0,0 +1,21 @@
+﻿namespace PasswordStorageApp.MauiClient.Services
+{
+    public interface IToasterService
+    {
+        void Success(string message);
+        void Success(string message, string title);
+
+       
+        void Warning(string message);
+        void Warning(string message, string title);
+
+
+        void Info(string message);
+        void Info(string message, string title);
+
+
+        void Error(string message);
+        void Error(string message, string title);
+
+    }
+}
