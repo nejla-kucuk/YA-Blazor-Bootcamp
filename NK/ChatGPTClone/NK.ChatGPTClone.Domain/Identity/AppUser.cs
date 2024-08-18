f6943a2eb48556467e1e7ed3c6a6b9f80607dc9c
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-18 14:02:01 +0300

    [Feat]: Create User

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Domain/Identity/AppUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NK.ChatGPTClone.Domain.Common;
+using NK.ChatGPTClone.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NK.ChatGPTClone.Domain.Identity
+{
+    public class AppUser : IdentityUser<Guid>, IEntity, ICreatedByEntity, IModifiedByEntity
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+
+        public DateTimeOffset CreatedOn { get; set; }
+        public string CreatedByUserId { get; set; }
+
+        public DateTimeOffset? ModifiedOn { get; set; }
+        public string? ModifiedByUserId { get; set; }
+
+        public ICollection<ChatSession> ChatSession { get; set; }
+    }
+}
