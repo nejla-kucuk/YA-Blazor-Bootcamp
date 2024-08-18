7e71307e074d14a36776532d551bcec339328cc1
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-18 13:59:50 +0300

    [Feat]: Create Enum / message type

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Domain/Enums/ChatMessageType.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace NK.ChatGPTClone.Domain.Enums
+{
+   public enum ChatMessageType
+    {
+        System = 1, // Sistem mesajı
+
+        User = 2, // Kullanıcı mesajı
+
+        Assistant = 3 // Mesaja verilen asistan yanıtı
+    }
+}
