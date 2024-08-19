510270622e84b6f712bb2a4a12b053e783f89d1d
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-19 18:11:47 +0300

    [Feat]: Create UserRole configuration

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Persistence/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NK.ChatGPTClone.Domain.Identity;
+
+namespace NK.ChatGPTClone.Persistence.Configurations
+{
+    public class UserRoleConfiguration : IEntityTypeConfiguration<AppUserRole>
+    {
+        public void Configure(EntityTypeBuilder<AppUserRole> builder)
+        {
+            // Primary key
+            builder.HasKey(r => new { r.UserId, r.RoleId });
+
+            // Maps to the AspNetUserRoles table
+            builder.ToTable("t_userroles");
+        }
+    }
+}
