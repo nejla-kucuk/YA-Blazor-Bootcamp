9fb0088e100718de75fbbe4567e11275b4364bdd
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-20 16:01:59 +0300

    [Update]: Configure DbContext with Npgsql using connection string from configuration

--- NK/ChatGPTClone/NK.ChatGPTClone.WebApi/Program.cs
+++ NK/ChatGPTClone/NK.ChatGPTClone.WebApi/Program.cs
@@ -1,11 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using NK.ChatGPTClone.Persistence.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseNpgsql(connectionString));
+
 
 var app = builder.Build();
 
