6b028347ee507d92a7581b9aac2d937c17129c13
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-20 15:56:27 +0300

    [Feat]: Add ApplicationDbContextFactory for design-time DbContext creation

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Persistence/Contexts/ApplicationDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace NK.ChatGPTClone.Persistence.Contexts
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+           
+           var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
