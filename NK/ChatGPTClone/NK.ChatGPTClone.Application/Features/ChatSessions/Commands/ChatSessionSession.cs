ad4a21f3e098f0ed450a553181bb18f0d341b755
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-21 22:39:44 +0300

    [Feat]: Add ChatSessionSession class to handle chat session creation

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/ChatSessions/Commands/ChatSessionSession.cs
@@ -0,0 +1,22 @@
+﻿using NK.ChatGPTClone.Application.Common.Interfaces;
+using NK.ChatGPTClone.Domain.Entities;
+
+namespace NK.ChatGPTClone.Application.Features.ChatSessions.Commands
+{
+    public class ChatSessionSession
+    {
+        public readonly IApplicationDbContext _applicationDbContext;
+
+        public ChatSessionSession(IApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task CreateChatSessionAsync(ChatSession chatSession, CancellationToken cancellationToken)
+        {
+            _applicationDbContext.ChatSessions.Add(chatSession);
+
+            await _applicationDbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
