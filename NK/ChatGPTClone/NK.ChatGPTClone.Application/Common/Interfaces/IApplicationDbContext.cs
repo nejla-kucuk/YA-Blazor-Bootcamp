bb6ff13a501c560969df8ea4ce5700b923bf7325
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-21 22:16:56 +0300

    [Feat]: Add IApplicationDbContext interface with ChatSession DbSet

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NK.ChatGPTClone.Domain.Entities;
+
+namespace NK.ChatGPTClone.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<ChatSession> ChatSessions { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+        int SaveChanges();
+    }
+}
