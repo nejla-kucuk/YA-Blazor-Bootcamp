6b840d56d54fd88f59dca17e410a4dd49bfe5d7d
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-22 11:46:47 +0300

    [Feat]: Add infrastructure setup with PostgreSQL

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NK.ChatGPTClone.Application.Common.Interfaces;
+using NK.ChatGPTClone.Infrastructure.Contexts;
+
+
+namespace NK.ChatGPTClone.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+
+            services.AddDbContext<ApplicationDbContext>(opt => opt.UseNpgsql(connectionString));
+
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+
+            return services;
+        }
+    }
+}
