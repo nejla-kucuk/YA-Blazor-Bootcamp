947562d17487d82bdca257c4a882bb4074aa77f0
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-22 11:49:05 +0300

    [Feat]: Add AppUser entity from Infrastructure layer

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Infrastructure/Identity/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NK.ChatGPTClone.Domain.Common;
+
+
+namespace NK.ChatGPTClone.Infrastructure.Identity
+{
+    public class AppUser : IdentityUser<Guid>, IEntity, ICreatedByEntity, IModifiedByEntity
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+
+        public DateTimeOffset CreatedOn { get; set; }
+        public string CreatedByUserId { get; set; }
+
+        public DateTimeOffset? ModifiedOn { get; set; }
+        public string? ModifiedByUserId { get; set; }
+
+        
+    }
+}
