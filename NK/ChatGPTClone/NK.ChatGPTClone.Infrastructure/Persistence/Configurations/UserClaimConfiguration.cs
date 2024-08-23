2714b54b6ebdeaa02a65111c851089b0df8a03bc
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-23 14:40:24 +0300

    [Feat]: Add UserClaim configuration for EF Core

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Infrastructure/Persistence/Configurations/UserClaimConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NK.ChatGPTClone.Domain.Identity;
+
+namespace NK.ChatGPTClone.Infrastructure.Configurations
+{
+    public class UserClaimConfiguration : IEntityTypeConfiguration<AppUserClaim>
+    {
+        public void Configure(EntityTypeBuilder<AppUserClaim> builder)
+        {
+            // Primary key
+            builder.HasKey(x => x.Id);
+
+            // Maps to the AspNetUserClaims table
+            builder.ToTable("t_userclaims");
+        }
+    }
+}
