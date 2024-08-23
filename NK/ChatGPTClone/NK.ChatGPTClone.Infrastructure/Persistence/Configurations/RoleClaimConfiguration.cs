0fa60bd8b8d83d9e656ba8bc12b6548d85110cda
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-23 14:40:07 +0300

    [Feat]: Add RoleClaim configuration for EF Core

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Infrastructure/Persistence/Configurations/RoleClaimConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using NK.ChatGPTClone.Domain.Identity;
+
+namespace NK.ChatGPTClone.Infrastructure.Configurations
+{
+    public class RoleClaimConfiguration : IEntityTypeConfiguration<RoleClaim>
+    {
+        public void Configure(EntityTypeBuilder<RoleClaim> builder)
+        {
+            // Primary key
+            builder.HasKey(rc => rc.Id);
+
+            // Maps to the AspNetRoleClaims table
+            builder.ToTable("t_roleclaims");
+        }
+    }
+}
