3a85ed5bb8a92c062244c38c13981c11bc40d7f6
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-23 14:41:44 +0300

    [Feat]: Add ApplicationDbContext with Identity configurations

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NK.ChatGPTClone.Application.Common.Interfaces;
+using NK.ChatGPTClone.Domain.Entities;
+using NK.ChatGPTClone.Infrastructure.Identity;
+
+
+namespace NK.ChatGPTClone.Infrastructure.Contexts
+{
+    public class ApplicationDbContext: IdentityDbContext<AppUser, Role, Guid, AppUserClaim, AppUserRole,AppUserLogin, RoleClaim, AppUserToken>, IApplicationDbContext
+    {
+
+        public DbSet<ChatSession> ChatSessions { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
