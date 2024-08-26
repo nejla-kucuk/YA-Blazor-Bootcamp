374c4c1c0f2a2a416eb04a87ee375b6523b0e69b
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-26 15:51:51 +0300

    [Feat]: Add ChatSessionGetByIdQuery with MediatR

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/ChatSessions/Queries/GetById/ChatSessionGetByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace NK.ChatGPTClone.Application.Features.ChatSessions.Queries.GetById
+{
+    public class ChatSessionGetByIdQuery: IRequest<ChatSessionGetByIdDto>
+    {
+        public Guid Id { get; set; }
+
+        public ChatSessionGetByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+
+
+}
