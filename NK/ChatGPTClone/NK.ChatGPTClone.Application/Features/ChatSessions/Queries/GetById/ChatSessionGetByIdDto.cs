8a551f6892e5215daded99cf6d9b3f0a9bfe3e41
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-26 15:52:35 +0300

    [Feat]: Add ChatSessionGetByIdDto with mapping method

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/ChatSessions/Queries/GetById/ChatSessionGetByIdDto.cs
@@ -0,0 +1,33 @@
+﻿using NK.ChatGPTClone.Domain.Entities;
+using NK.ChatGPTClone.Domain.Enums;
+using NK.ChatGPTClone.Domain.ValueObjects;
+
+namespace NK.ChatGPTClone.Application.Features.ChatSessions.Queries.GetById
+{
+    public sealed class ChatSessionGetByIdDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public GptModelType Model { get; set; }
+
+        public List<ChatThread> Threads { get; set; }
+
+        public Guid AppUserId { get; set; }
+
+        public DateTimeOffset CreatedOn { get; set; }
+
+        public static ChatSessionGetByIdDto MapFromChatSession(ChatSession chatSession) 
+        { 
+            return new ChatSessionGetByIdDto() 
+            { 
+                Id = chatSession.Id, 
+                Title = chatSession.Title, 
+                Model = chatSession.Model, 
+                Threads = chatSession.Threads, 
+                AppUserId = chatSession.AppUserId, 
+                CreatedOn = chatSession.CreatedOn 
+            };
+        }
+    }
+}
