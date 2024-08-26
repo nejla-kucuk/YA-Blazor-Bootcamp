c4698ca46396987f384c2cf2bdf2a3c6c74b3c96
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-26 15:51:15 +0300

    [Feat]: Add ChatSessionGetByIdQueryHandler with MediatR

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/ChatSessions/Queries/GetById/ChatSessionGetByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NK.ChatGPTClone.Application.Common.Interfaces;
+
+namespace NK.ChatGPTClone.Application.Features.ChatSessions.Queries.GetById
+{
+   public class ChatSessionGetByIdQueryHandler: IRequestHandler<ChatSessionGetByIdQuery, ChatSessionGetByIdDto>
+   {
+        private readonly IApplicationDbContext _context;
+
+        public async Task<ChatSessionGetByIdDto> Handle(ChatSessionGetByIdQuery request, CancellationToken cancellationToken)
+        {
+            var chatSession = await _context.ChatSessions
+                 .AsNoTracking()
+                 .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            return ChatSessionGetByIdDto.MapFromChatSession(chatSession);
+        }
+   }
+
+}
