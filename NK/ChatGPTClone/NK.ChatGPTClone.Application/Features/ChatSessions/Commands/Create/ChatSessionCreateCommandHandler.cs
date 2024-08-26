e93465a600577f2a50994e1bd14b55ea9cf7fb1f
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-26 15:53:26 +0300

    [Feat]: Add ChatSessionCreateCommandHandler with MediatR

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/ChatSessions/Commands/Create/ChatSessionCreateCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using NK.ChatGPTClone.Application.Common.Interfaces;
+
+namespace NK.ChatGPTClone.Application.Features.ChatSessions.Commands.Create
+{
+    public class ChatSessionCreateCommandHandler: IRequestHandler<ChatSessionCreateCommand, Guid>
+    {
+
+        private readonly IApplicationDbContext _context;
+        private readonly ICurrentUserService _currentUserService;
+
+        public ChatSessionCreateCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Guid> Handle(ChatSessionCreateCommand request, CancellationToken cancellationToken)
+        {
+            var chatSession = request.ToChatSession(_currentUserService.UserId);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return chatSession.Id;
+        }
+    }
+}
