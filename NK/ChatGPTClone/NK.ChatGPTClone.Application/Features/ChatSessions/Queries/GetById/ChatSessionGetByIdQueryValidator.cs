776145fc30075e43154285168dda6c7631f4fea6
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-26 15:50:04 +0300

    [Feat]: Add ChatSessionGetByIdQueryValidator with FluentValidation

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/ChatSessions/Queries/GetById/ChatSessionGetByIdQueryValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using NK.ChatGPTClone.Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace NK.ChatGPTClone.Application.Features.ChatSessions.Queries.GetById
+{
+    public class ChatSessionGetByIdQueryValidator:AbstractValidator<ChatSessionGetByIdQuery>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public ChatSessionGetByIdQueryValidator(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            RuleFor(p => p.Id)
+                .NotEmpty()
+                .NotNull()
+                .MustAsync(BeValidIdAsync)
+                .WithMessage("The selected Chat was not found.");
+        }
+
+        private Task<bool> BeValidIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return _dbContext
+                .ChatSessions
+                .AnyAsync(x => x.Id == id, cancellationToken);
+        }
+    }
+}
