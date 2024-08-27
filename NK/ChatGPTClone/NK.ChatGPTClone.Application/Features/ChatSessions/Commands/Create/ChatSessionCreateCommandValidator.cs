f4591b900ef17969937b39a65807a2b98090baf5
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-27 23:21:49 +0300

    [Feat]: Add ChatSessionCreateCommandValidator with FluentValidation

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/ChatSessions/Commands/Create/ChatSessionCreateCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace NK.ChatGPTClone.Application.Features.ChatSessions.Commands.Create
+{
+    public class ChatSessionCreateCommandValidator:AbstractValidator<ChatSessionCreateCommand>
+    {
+
+        public ChatSessionCreateCommandValidator() 
+        {
+            RuleFor(x => x.Model)
+                .NotNull()
+                .WithMessage("Model is required")
+                .NotEmpty()
+                .WithMessage("Model is required")
+                .IsInEnum()
+                .WithMessage("Model is invalid");
+
+
+            RuleFor(x => x.Content)
+                .NotNull()
+                .WithMessage("Content is required")
+                .NotEmpty()
+                .WithMessage("Content is required")
+                .MaximumLength(4000)
+                .WithMessage("Content must not exceed 500 characters");
+
+        }
+
+    }
+}
