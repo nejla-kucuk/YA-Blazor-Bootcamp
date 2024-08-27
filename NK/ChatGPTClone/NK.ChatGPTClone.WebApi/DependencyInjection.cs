72a9626aeda879c856da506b587876d4eec80b91
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-27 23:48:03 +0300

    [Feat]: Add dependency injection for ICurrentUserService

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.WebApi/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using NK.ChatGPTClone.Application.Common.Interfaces;
+using NK.ChatGPTClone.WebApi.Services;
+
+namespace NK.ChatGPTClone.WebApi
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddWebApi(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICurrentUserService, CurrentUserManager>();
+
+            return services;
+        }
+    }
+}
