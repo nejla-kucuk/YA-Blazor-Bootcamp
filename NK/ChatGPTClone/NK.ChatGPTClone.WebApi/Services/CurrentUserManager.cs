48cb5ce773ad6dd4f80d775f3b0e2e1ba7512a70
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-27 23:47:25 +0300

    [Feat]: Add CurrentUserManager service implementation

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.WebApi/Services/CurrentUserManager.cs
@@ -0,0 +1,27 @@
+﻿using NK.ChatGPTClone.Application.Common.Interfaces;
+using System.Security.Claims;
+
+namespace NK.ChatGPTClone.WebApi.Services
+{
+    public class CurrentUserManager : ICurrentUserService
+    {
+        public readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserManager(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Guid UserId { get; set; }
+
+        private Guid GetUserId()
+        {
+            var userId = _httpContextAccessor
+                         .HttpContext?
+                         .User?
+                         .FindFirstValue("uid");
+            
+            return string.IsNullOrEmpty(userId) ? Guid.Empty : Guid.Parse(userId);
+        }
+    }
+}
