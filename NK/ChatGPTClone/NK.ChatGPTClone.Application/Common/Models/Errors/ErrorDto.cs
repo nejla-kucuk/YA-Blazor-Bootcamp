ed235121a103596b6208ed487e60e050a0db00d0
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-28 16:14:14 +0300

    [Feat]: Add ErrorDto class for error handling

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Common/Models/Errors/ErrorDto.cs
@@ -0,0 +1,16 @@
+﻿namespace NK.ChatGPTClone.Application.Common.Models.Errors
+{
+    public class ErrorDto
+    {
+        public string PropertyName { get; set; }
+
+        public IReadOnlyCollection<string> Messages { get; set; }
+
+        public ErrorDto(string propertyName, List<string> messages)
+        {
+            PropertyName = propertyName;
+
+            Messages = messages;
+        }
+    }
+}
