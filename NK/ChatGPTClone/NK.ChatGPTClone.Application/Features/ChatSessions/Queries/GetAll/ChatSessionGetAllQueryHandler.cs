d4c32e852dde8c4de009f90bf0b854802f42db67
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-28 16:20:07 +0300

    [Feat]: Add ChatSessionGetAllQueryHandler with MediatR

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/ChatSessions/Queries/GetAll/ChatSessionGetAllQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NK.ChatGPTClone.Application.Common.Interfaces;
+
+namespace NK.ChatGPTClone.Application.Features.ChatSessions.Queries.GetAll
+{
+   public class ChatSessionGetAllQueryHandler : IRequestHandler<ChatSessionGetAllQuery, List<ChatSessionGetAllDto>>
+   {
+        private readonly IApplicationDbContext _context;
+        private readonly ICurrentUserService _currentUserService;
+
+        public ChatSessionGetAllQueryHandler(IApplicationDbContext context, ICurrentUserService currentUserService)
+        {
+            _context = context;
+            _currentUserService = currentUserService;
+        }
+
+        public Task<List<ChatSessionGetAllDto>> Handle(ChatSessionGetAllQuery request, CancellationToken cancellationToken)
+        {
+            return _context
+                .ChatSessions
+                .AsNoTracking()
+                .Where(x => x.AppUserId == _currentUserService.UserId)
+                .OrderByDescending(cs => cs.CreatedOn)
+                .Select(x => ChatSessionGetAllDto.MapFromChatSession(x))
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
