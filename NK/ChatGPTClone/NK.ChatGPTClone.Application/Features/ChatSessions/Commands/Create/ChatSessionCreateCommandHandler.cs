48356ff96b0614e5f0754425c4a262cf328a4882
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-28 16:17:46 +0300

    [Refactor]: Update ChatSessionCreateCommandHandler to return ResponseDto<Guid>

--- NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/ChatSessions/Commands/Create/ChatSessionCreateCommandHandler.cs
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/ChatSessions/Commands/Create/ChatSessionCreateCommandHandler.cs
@@ -1,9 +1,10 @@
 ﻿using MediatR;
 using NK.ChatGPTClone.Application.Common.Interfaces;
+using NK.ChatGPTClone.Application.Common.Models.General;
 
 namespace NK.ChatGPTClone.Application.Features.ChatSessions.Commands.Create
 {
-    public class ChatSessionCreateCommandHandler: IRequestHandler<ChatSessionCreateCommand, Guid>
+    public class ChatSessionCreateCommandHandler: IRequestHandler<ChatSessionCreateCommand, ResponseDto<Guid>>
     {
 
         private readonly IApplicationDbContext _context;
@@ -14,13 +15,13 @@ namespace NK.ChatGPTClone.Application.Features.ChatSessions.Commands.Create
             _context = context;
         }
 
-        public async Task<Guid> Handle(ChatSessionCreateCommand request, CancellationToken cancellationToken)
+        public async Task<ResponseDto<Guid>> Handle(ChatSessionCreateCommand request, CancellationToken cancellationToken)
         {
             var chatSession = request.ToChatSession(_currentUserService.UserId);
 
             await _context.SaveChangesAsync(cancellationToken);
 
-            return chatSession.Id;
+            return new ResponseDto<Guid>(chatSession.Id, "A new chat session was created successfuly.");
         }
     }
 }
