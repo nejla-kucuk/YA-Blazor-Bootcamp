3e5322bba829f459c4af734a9427c7bedd98ff50
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-28 16:15:29 +0300

    [Feat]: Add dependency injection for FluentValidation and MediatR

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/DependenceInjection.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace NK.ChatGPTClone.Application
+{
+    public static class DependenceInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(config=>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            return services;
+        }
+    }
+}
