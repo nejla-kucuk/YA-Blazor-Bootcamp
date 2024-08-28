56f8f5a87c76ebecafa70df93cb6fb86e5cdd33a
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-28 16:20:53 +0300

    [Feat]: Add ApiControllerBase with MediatR support

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.WebApi/Controllers/ApiControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NK.ChatGPTClone.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiControllerBase : ControllerBase
+    {
+
+        protected readonly ISender Mediatr;
+
+        public ApiControllerBase(ISender mediatr)
+        {
+            Mediatr = mediatr;
+        }
+    }
+}
