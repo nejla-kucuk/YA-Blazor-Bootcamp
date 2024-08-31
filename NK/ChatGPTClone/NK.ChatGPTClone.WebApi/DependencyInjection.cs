c8a7700622973828cd1c97ffda3e7f680d806b98
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-31 18:38:03 +0300

    [Update]: Added localization support and configured request localization

--- NK/ChatGPTClone/NK.ChatGPTClone.WebApi/DependencyInjection.cs
+++ NK/ChatGPTClone/NK.ChatGPTClone.WebApi/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using NK.ChatGPTClone.Application.Common.Interfaces;
+﻿using Microsoft.AspNetCore.Localization;
+using NK.ChatGPTClone.Application.Common.Interfaces;
 using NK.ChatGPTClone.WebApi.Services;
+using System.Globalization;
 
 namespace NK.ChatGPTClone.WebApi
 {
@@ -10,6 +12,30 @@ namespace NK.ChatGPTClone.WebApi
             services.AddHttpContextAccessor();
 
             services.AddScoped<ICurrentUserService, CurrentUserManager>();
+
+            //Localization
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                // Set the default culture
+                var defaultCulture = new CultureInfo("en-US");
+
+                // Set supported cultures
+                var supportedCultures = new List<CultureInfo>
+                {
+                    defaultCulture,
+                    new CultureInfo("tr-TR")
+                };
+
+                // Add supported cultures
+                options.DefaultRequestCulture = new RequestCulture(defaultCulture);
+
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+                options.ApplyCurrentCultureToResponseHeaders = true;
+                
+            });
 
             return services;
         }
