9c398b8974db239724ff64d82d7118e9375792b0
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-08-31 18:40:29 +0300

    Update Program.cs

--- NK/ChatGPTClone/NK.ChatGPTClone.WebApi/Program.cs
+++ NK/ChatGPTClone/NK.ChatGPTClone.WebApi/Program.cs
@@ -1,40 +1,73 @@
+using Microsoft.Extensions.Options;
 using NK.ChatGPTClone.Application;
 using NK.ChatGPTClone.Infrastructure;
 using NK.ChatGPTClone.WebApi;
+using NK.ChatGPTClone.WebApi.Filters;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .MinimumLevel.Warning()
+    .CreateLogger();
 
-// Add services to the container.
-
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddApplication();
-
-builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.Services.AddWebApi(builder.Configuration);
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    builder.Services.AddSerilog();
+    builder.Services.AddControllers(opt =>
+    {
+        opt.Filters.Add<GlobalExceptionFilter>();
+    });
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddApplication();
+
+    builder.Services.AddInfrastructure(builder.Configuration);
+
+    builder.Services.AddWebApi(builder.Configuration);
+
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    var requestLocalizationOptions = app.Services
+        .GetRequiredService<IOptions<RequestLocalizationOptions>>().Value;
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthentication();
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
