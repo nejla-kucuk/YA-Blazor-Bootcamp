dfdcf8c0c6364dd7bd96548df68afe60eef09823
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-01 21:16:33 +0300

    [Feat]: Add IIdentityService interface with authentication and login methods.

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,12 @@
+﻿using NK.ChatGPTClone.Application.Common.Models.Identity;
+
+namespace NK.ChatGPTClone.Application.Common.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<bool> AuthenticateAsync(IdentityAuthenticateRequest request, CancellationToken cancellationToken);
+        
+        Task<IdentityLoginResponse> LoginAsync(IdentityLoginRequest request, CancellationToken cancellationToken);
+
+    }
+}
