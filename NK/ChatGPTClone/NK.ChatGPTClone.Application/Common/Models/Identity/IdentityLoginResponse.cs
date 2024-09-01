50acaab00e8f7d56351f235069960c7bae2bd95f
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-01 21:20:15 +0300

    [Feat]: Add IdentityLoginResponse model with token and expiration properties.

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Common/Models/Identity/IdentityLoginResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace NK.ChatGPTClone.Application.Common.Models.Identity
+{
+    public class IdentityLoginResponse
+    {
+        public string Token { get; set; }
+
+        public DateTime ExpiresAt { get; set; }
+
+        public IdentityLoginResponse(string token, DateTime expiresAt)
+        {
+            Token = token;
+            ExpiresAt = expiresAt;
+        }
+    }
+}
