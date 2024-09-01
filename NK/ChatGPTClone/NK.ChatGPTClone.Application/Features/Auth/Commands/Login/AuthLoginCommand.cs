622ea328e80cfc7aaf948fa35efd92a1eac5cf69
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-01 21:21:42 +0300

    [Feat]: Add AuthLoginCommand with request conversion methods

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/Auth/Commands/Login/AuthLoginCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using NK.ChatGPTClone.Application.Common.Models.General;
+using NK.ChatGPTClone.Application.Common.Models.Identity;
+
+namespace NK.ChatGPTClone.Application.Features.Auth.Commands.Login
+{
+    public class AuthLoginCommand : IRequest<ResponseDto<AuthLoginDto>>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public AuthLoginCommand(string email, string password)
+        {
+            Email = email;
+
+            Password = password;
+        }
+
+        public IdentityLoginRequest ToIdentityLoginRequest()
+        {
+            return new IdentityLoginRequest(Email, Password);
+        }
+
+        public IdentityAuthenticateRequest ToIdentityAuthenticateRequest()
+        {
+            return new IdentityAuthenticateRequest(Email, Password);
+        }
+    }
+}
