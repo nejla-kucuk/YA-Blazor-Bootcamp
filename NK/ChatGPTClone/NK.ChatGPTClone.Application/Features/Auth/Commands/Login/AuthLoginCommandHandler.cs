64f66d8fc4194a2b556b8b2ebfc1a301b4796ca4
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-01 21:22:04 +0300

    [Feat]: Add AuthLoginCommandHandler to handle login requests.

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/Auth/Commands/Login/AuthLoginCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using NK.ChatGPTClone.Application.Common.Interfaces;
+using NK.ChatGPTClone.Application.Common.Models.General;
+
+
+namespace NK.ChatGPTClone.Application.Features.Auth.Commands.Login
+{
+    public class AuthLoginCommandHandler : IRequestHandler<AuthLoginCommand, ResponseDto<AuthLoginDto>>
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthLoginCommandHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<ResponseDto<AuthLoginDto>> Handle(AuthLoginCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _identityService.LoginAsync(request.ToIdentityLoginRequest(), cancellationToken);
+
+            return new ResponseDto<AuthLoginDto>(AuthLoginDto.FromIdentityLoginResponse(response));
+        }
+    }
+}
