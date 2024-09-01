9e5fb72a6cd5a1cc2d6becb8eb4e0487fce8b991
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-01 21:16:55 +0300

    [Feat]: Add IJwtService interface with token generation method.

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Common/Interfaces/IJwtService.cs
@@ -0,0 +1,9 @@
+﻿using NK.ChatGPTClone.Application.Common.Models.Jwt;
+
+namespace NK.ChatGPTClone.Application.Common.Interfaces
+{
+    public interface IJwtService
+    {
+        JwtGenerateTokenResponse GenerateToken(JwtGenerateTokenRequest request);
+    }
+}
