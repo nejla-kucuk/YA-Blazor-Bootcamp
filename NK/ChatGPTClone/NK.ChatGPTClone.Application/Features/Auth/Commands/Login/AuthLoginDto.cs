5ef15aaf282fe15303976defcadd186f6320a9a0
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-02 15:12:34 +0300

    [Feat]: Add AuthLoginDto class for handling authentication responses

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/Auth/Commands/Login/AuthLoginDto.cs
@@ -0,0 +1,21 @@
+﻿using NK.ChatGPTClone.Application.Common.Models.Identity;
+
+namespace NK.ChatGPTClone.Application.Features.Auth.Commands.Login
+{
+    public class AuthLoginDto
+   {
+        public string Token { get; set; }
+        public DateTime ExpiresAt { get; set; }
+
+        public AuthLoginDto(string token, DateTime expiresAt)
+        {
+            Token = token;
+            ExpiresAt = expiresAt;
+        }
+
+        public static AuthLoginDto FromIdentityLoginResponse(IdentityLoginResponse response)
+        {
+            return new AuthLoginDto(response.Token, response.ExpiresAt);
+        }
+    }
+}
