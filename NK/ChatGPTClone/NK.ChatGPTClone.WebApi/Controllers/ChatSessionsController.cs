2950c52bdfeb5dde621aefd60eb6591044b9afcb
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-02 15:30:41 +0300

    [Update]: Update ChatSessionsController to include localization and fix GetAllAsync method

--- NK/ChatGPTClone/NK.ChatGPTClone.WebApi/Controllers/ChatSessionsController.cs
+++ NK/ChatGPTClone/NK.ChatGPTClone.WebApi/Controllers/ChatSessionsController.cs
@@ -1,23 +1,25 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using NK.ChatGPTClone.Application.Features.ChatSessions.Commands.Create;
 using NK.ChatGPTClone.Application.Features.ChatSessions.Queries.GetAll;
 using NK.ChatGPTClone.Application.Features.ChatSessions.Queries.GetById;
+using NK.ChatGPTClone.WebApi.Filters;
 
 namespace NK.ChatGPTClone.WebApi.Controllers
 {
     public class ChatSessionsController : ApiControllerBase
     {
-        public ChatSessionsController(ISender mediatr): base(mediatr)
+        private readonly IStringLocalizer<GlobalExceptionFilter> _localizer;
+        public ChatSessionsController(ISender mediatr, IStringLocalizer<GlobalExceptionFilter> localizer) : base(mediatr)
         {
-            
+            _localizer = localizer;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
         {
-          
-            return Ok(await Mediatr.Send(new ChatSessionGetAllQuery(),cancellationToken));
+            return Ok(await Mediatr.Send(new ChatSessionGetAllQuery(), cancellationToken));
         }
 
         [HttpGet("{id}")]
@@ -31,6 +33,5 @@ namespace NK.ChatGPTClone.WebApi.Controllers
         {
             return Ok(await Mediatr.Send(command, cancellationToken));
         }
-
     }
 }
