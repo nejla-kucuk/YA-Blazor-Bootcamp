37eecb27479a319b1968ade74f5b7860d4bd1cb6
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-02 15:33:00 +0300

    [Update]: Update CurrentUserManager to use IHttpContextAccessor for retrieving UserId

--- NK/ChatGPTClone/NK.ChatGPTClone.WebApi/Services/CurrentUserManager.cs
+++ NK/ChatGPTClone/NK.ChatGPTClone.WebApi/Services/CurrentUserManager.cs
@@ -5,19 +5,25 @@ namespace NK.ChatGPTClone.WebApi.Services
 {
     public class CurrentUserManager : ICurrentUserService
     {
-        public readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public CurrentUserManager(IHttpContextAccessor httpContextAccessor)
         {
             _httpContextAccessor = httpContextAccessor;
         }
 
-        public Guid UserId { get; set; }
+        public Guid UserId => GetUserId();
 
         private Guid GetUserId()
         {
-
             return Guid.Parse("2798212b-3e5d-4556-8629-a64eb70da4a8");
+
+            var userId = _httpContextAccessor
+                .HttpContext?
+                .User?
+                .FindFirstValue("uid");
+
+            return string.IsNullOrEmpty(userId) ? Guid.Empty : Guid.Parse(userId);
         }
     }
 }
