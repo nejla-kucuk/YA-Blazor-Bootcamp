3b2e2cc11ec224638313a9eaabc89eff2f2313e6
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-02 15:26:41 +0300

    [Feat]: Add IdentityManager class to handle authentication and JWT token generation

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Infrastructure/Services/IdentityManager.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NK.ChatGPTClone.Application.Common.Interfaces;
+using NK.ChatGPTClone.Application.Common.Models.Identity;
+using NK.ChatGPTClone.Application.Common.Models.Jwt;
+using NK.ChatGPTClone.Infrastructure.Identity;
+
+namespace NK.ChatGPTClone.Infrastructure.Services
+{
+    public class IdentityManager : IIdentityService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IJwtService _jwtService;
+
+        public IdentityManager(UserManager<AppUser> userManager, IJwtService jwtService)
+        {
+            _userManager = userManager;
+            _jwtService = jwtService;
+        }
+
+        public async Task<bool> AuthenticateAsync(IdentityAuthenticateRequest request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+
+            if (user is null) return false;
+
+            return await _userManager.CheckPasswordAsync(user, request.Password);
+        }
+
+        public async Task<IdentityLoginResponse> LoginAsync(IdentityLoginRequest request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var jwtRequest = new JwtGenerateTokenRequest(user.Id, user.Email, roles);
+
+            var jwtResponse = _jwtService.GenerateToken(jwtRequest);
+
+            return new IdentityLoginResponse(jwtResponse.Token, jwtResponse.ExpiresAt);
+        }
+    }
+}
