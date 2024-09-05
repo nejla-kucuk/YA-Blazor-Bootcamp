c36198cc1854098f685f369617dcb547fd5263d2
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-05 22:53:51 +0300

    [Feat]: Added AuthRegisterCommandValidator with email and password validation rules

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/Auth/Commands/Register/AuthRegisterCommandValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using NK.ChatGPTClone.Application.Common.Interfaces;
+
+namespace NK.ChatGPTClone.Application.Features.Auth.Commands.Register
+{
+    public class AuthRegisterCommandValidator : AbstractValidator<AuthRegisterCommand>
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthRegisterCommandValidator() 
+        {
+            RuleFor(x => x.Email)
+             .NotEmpty()
+             .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(6);
+
+            RuleFor(x => x.FirstName)
+                .MaximumLength(50);
+
+            RuleFor(x => x.LastName)
+               .MaximumLength(50);
+
+            RuleFor(x => x.Email)
+                .MustAsync(CheckEmailExistsAsync)
+                .WithMessage("The email is already exists.");
+
+        }
+
+        private async Task<bool> CheckEmailExistsAsync(string email, CancellationToken cancellationToken)
+        {
+            return !await _identityService.CheckEmailExistsAsync(email, cancellationToken);
+        }
+
+    }
+}
