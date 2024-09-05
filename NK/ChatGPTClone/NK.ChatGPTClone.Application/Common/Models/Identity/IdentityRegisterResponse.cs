a21b4879f0bc4f4c99dce41002b413270e7191e8
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-05 22:52:55 +0300

    [Feat]: Added IdentityRegisterResponse model to handle user registration responses

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Common/Models/Identity/IdentityRegisterResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace NK.ChatGPTClone.Application.Common.Models.Identity
+{
+    public class IdentityRegisterResponse
+    {
+        public Guid Id { get; set; }
+        public string EmailToken { get; set; }
+
+        public IdentityRegisterResponse(Guid id, string emailToken)
+        {
+            EmailToken = emailToken;
+            Id = id;
+        }
+    }
+}
