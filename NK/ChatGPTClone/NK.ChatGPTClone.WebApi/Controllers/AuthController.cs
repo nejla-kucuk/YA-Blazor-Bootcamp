69ea08d26f7136ca3c237d86ed13c3a4261fdbb6
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-06 20:41:04 +0300

    [Feat]: Add AuthController with login endpoint

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NK.ChatGPTClone.Application.Features.Auth.Commands.Login;
+
+namespace NK.ChatGPTClone.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ApiControllerBase
+    {
+        public AuthController(ISender mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login (AuthLoginCommand command, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediatr.Send(command,cancellationToken));
+        }
+    }
+}
