5a2617c7ffbe1d1b5c9b3c3e885b96f53da8b518
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-07 13:44:01 +0300

    [Feat]: Add AuthRegisterCommandHandler for user registration

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/Auth/Commands/Register/AuthRegisterCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using NK.ChatGPTClone.Application.Common.Interfaces;
+using NK.ChatGPTClone.Application.Common.Models.General;
+
+namespace NK.ChatGPTClone.Application.Features.Auth.Commands.Register
+{
+    public class AuthRegisterCommandHandler : IRequestHandler<AuthRegisterCommand, ResponseDto<AuthRegisterDto>>
+    {
+        private readonly IIdentityService _identityService;
+
+
+        public AuthRegisterCommandHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<ResponseDto<AuthRegisterDto>> Handle(AuthRegisterCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _identityService.RegisterAsync(request.ToIdentityRegisterRequest(), cancellationToken);
+
+            return new ResponseDto<AuthRegisterDto>(AuthRegisterDto.Create(response), "User registered successfully.");
+        }
+    }
+}
