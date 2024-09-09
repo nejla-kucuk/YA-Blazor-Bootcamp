7fe56a4ab36b5458d32c49a635c93f3b3bd82147
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-09 22:32:33 +0300

    Feat: Implement EnvironmentManager for managing web root path

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.WebApi/Services/EnvironmentManager.cs
@@ -0,0 +1,14 @@
+﻿using NK.ChatGPTClone.Application.Common.Interfaces;
+
+namespace NK.ChatGPTClone.WebApi.Services
+{
+    public class EnvironmentManager: IEnvironmentService
+    {
+        public string WebRootPath { get; }
+
+        public EnvironmentManager(string webRootPath)
+        {
+            WebRootPath = webRootPath;
+        }
+    }
+}
