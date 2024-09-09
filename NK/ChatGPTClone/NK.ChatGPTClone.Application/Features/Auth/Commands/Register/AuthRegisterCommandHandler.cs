fe751d179e65597c265f62f608914ccf5dad9f27
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-09 22:29:04 +0300

    Update: Inject IEmailService into AuthRegisterCommandHandler and send verification email

--- NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/Auth/Commands/Register/AuthRegisterCommandHandler.cs
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/Auth/Commands/Register/AuthRegisterCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using NK.ChatGPTClone.Application.Common.Interfaces;
+using NK.ChatGPTClone.Application.Common.Models.Email;
 using NK.ChatGPTClone.Application.Common.Models.General;
 
 namespace NK.ChatGPTClone.Application.Features.Auth.Commands.Register
@@ -8,15 +9,20 @@ namespace NK.ChatGPTClone.Application.Features.Auth.Commands.Register
     {
         private readonly IIdentityService _identityService;
 
+        private readonly IEmailService _emailService;
 
-        public AuthRegisterCommandHandler(IIdentityService identityService)
+
+        public AuthRegisterCommandHandler(IIdentityService identityService, IEmailService emailService)
         {
             _identityService = identityService;
+            _emailService = emailService;
         }
 
         public async Task<ResponseDto<AuthRegisterDto>> Handle(AuthRegisterCommand request, CancellationToken cancellationToken)
         {
             var response = await _identityService.RegisterAsync(request.ToIdentityRegisterRequest(), cancellationToken);
+
+            await _emailService.EmailVerificationAsync(new EmailVerificationDto(response.Email, response.EmailToken), cancellationToken);
 
             return new ResponseDto<AuthRegisterDto>(AuthRegisterDto.Create(response), "User registered successfully.");
         }
