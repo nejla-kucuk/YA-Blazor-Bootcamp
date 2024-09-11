e52c536d5133b0bb254fbe0d55dd9a10f382a367
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-11 17:12:30 +0300

    Add IdentityVerifyEmailResponse model

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Common/Models/Identity/IdentityVerifyEmailResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace NK.ChatGPTClone.Application.Common.Models.Identity
+{
+    public class IdentityVerifyEmailResponse
+    {
+        public string Email { get; set; }   
+
+        public IdentityVerifyEmailResponse(string email)
+        {
+            Email = email;
+        }
+    }
+}
