88771147063db5212aaf5b3e95109f3f99120f9d
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-11 17:12:50 +0300

    Add AuthReSendEmailVerificationEmailCommand model

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/Auth/Commands/ReSendEmailVerificationEmail/AuthReSendEmailVerificationEmailCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using NK.ChatGPTClone.Application.Common.Models.General;
+
+namespace NK.ChatGPTClone.Application.Features.Auth.Commands.ReSendEmailVerificationEmail
+{
+    public class AuthReSendEmailVerificationEmailCommand:IRequest<ResponseDto<string>>
+    {
+        public string Email { get; set; }
+
+        public AuthReSendEmailVerificationEmailCommand(string email)
+        {
+            Email = email;
+        }
+    }
+}
