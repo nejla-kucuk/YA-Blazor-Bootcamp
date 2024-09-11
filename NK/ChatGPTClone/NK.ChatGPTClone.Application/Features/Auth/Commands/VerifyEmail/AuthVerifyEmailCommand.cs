b5e4b7c5ebb60aa790dd910a6132470edd65d206
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-11 17:14:13 +0300

    Add AuthVerifyEmailCommand model

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/Auth/Commands/VerifyEmail/AuthVerifyEmailCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using NK.ChatGPTClone.Application.Common.Models.General;
+
+namespace NK.ChatGPTClone.Application.Features.Auth.Commands.VerifyEmail
+{
+    public class AuthVerifyEmailCommand:IRequest<ResponseDto<string>>
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+
+        public AuthVerifyEmailCommand(string email, string token)
+        {
+            Email = email;
+            Token = token;
+        }
+    }
+}
