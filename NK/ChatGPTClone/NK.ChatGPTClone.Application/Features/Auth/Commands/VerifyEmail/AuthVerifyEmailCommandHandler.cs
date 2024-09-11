d6b848f7e9e2a47aaaf3a7e172fda508e9a802a4
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-12 00:05:35 +0300

    Add AuthVerifyEmailCommandHandler implementation

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/Auth/Commands/VerifyEmail/AuthVerifyEmailCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using NK.ChatGPTClone.Application.Common.Interfaces;
+using NK.ChatGPTClone.Application.Common.Models.General;
+using NK.ChatGPTClone.Application.Common.Models.Identity;
+
+namespace NK.ChatGPTClone.Application.Features.Auth.Commands.VerifyEmail
+{
+
+    public class AuthVerifyEmailCommandHandler : IRequestHandler<AuthVerifyEmailCommand, ResponseDto<string>>
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthVerifyEmailCommandHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<ResponseDto<string>> Handle(AuthVerifyEmailCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _identityService.VerifyEmailAsync(new IdentityVerifyEmailRequest(request.Email, request.Token), cancellationToken);
+
+            return new ResponseDto<string>(data: response.Email, message: "Email verified successfully.");
+        }
+    }
+}
