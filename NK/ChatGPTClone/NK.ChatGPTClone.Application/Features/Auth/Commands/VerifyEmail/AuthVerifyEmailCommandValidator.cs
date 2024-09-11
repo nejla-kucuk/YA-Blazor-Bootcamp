c6494f7591da3e09c303fb34c3809e86ce01ecfe
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-12 00:06:39 +0300

    Add AuthVerifyEmailCommandValidator implementation

---
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Features/Auth/Commands/VerifyEmail/AuthVerifyEmailCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using NK.ChatGPTClone.Application.Common.Interfaces;
+using System.Security.Principal;
+
+namespace NK.ChatGPTClone.Application.Features.Auth.Commands.VerifyEmail
+{
+    public class AuthVerifyEmailCommandValidator : AbstractValidator<AuthVerifyEmailCommand>
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthVerifyEmailCommandValidator(IIdentityService identityService)
+        {
+            _identityService = identityService;
+
+            RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MustAsync(EmailExists)
+            .WithMessage("Email not found.");
+
+            RuleFor(x => x.Token)
+            .NotEmpty()
+            .MinimumLength(20)
+            .WithMessage("Token is invalid.");
+
+        }
+
+        private Task<bool> EmailExists(string email, CancellationToken cancellationToken)
+        {
+            return _identityService.CheckEmailExistsAsync(email, cancellationToken);
+        }
+    }
+}
