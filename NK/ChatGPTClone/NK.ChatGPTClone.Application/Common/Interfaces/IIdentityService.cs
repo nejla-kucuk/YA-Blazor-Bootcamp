ec0fa0275518eeb55be5a342cfb93d0aab64e0cf
Nejla Kucuk <132754595+nejla-kucuk@users.noreply.github.com>
2024-09-11 17:09:53 +0300

    Add email verification and authentication methods to IIdentityService interface

--- NK/ChatGPTClone/NK.ChatGPTClone.Application/Common/Interfaces/IIdentityService.cs
+++ NK/ChatGPTClone/NK.ChatGPTClone.Application/Common/Interfaces/IIdentityService.cs
@@ -5,12 +5,19 @@ namespace NK.ChatGPTClone.Application.Common.Interfaces
     public interface IIdentityService
     {
         Task<bool> AuthenticateAsync(IdentityAuthenticateRequest request, CancellationToken cancellationToken);
-        
-        Task<IdentityLoginResponse> LoginAsync(IdentityLoginRequest request, CancellationToken cancellationToken);
 
         Task<bool> CheckEmailExistsAsync(string email, CancellationToken cancellationToken);
 
+        Task<bool> CheckIfEmailVerifiedAsync(string email, CancellationToken cancellationToken);
+
         Task<IdentityRegisterResponse> RegisterAsync(IdentityRegisterRequest request, CancellationToken cancellationToken);
+
+        Task<IdentityLoginResponse> LoginAsync(IdentityLoginRequest request, CancellationToken cancellationToken);
+
+        Task<IdentityVerifyEmailResponse> VerifyEmailAsync(IdentityVerifyEmailRequest request, CancellationToken cancellationToken);
+
+        Task<IdentityCreateEmailTokenResponse> CreateEmailTokenAsync(IdentityCreateEmailTokenRequest request, CancellationToken cancellationToken);
+
 
     }
 }
